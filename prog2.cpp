d6937cb025322b995df3971b10cedb5f0e700015
Jack Liu <jackliu@umail.ucsb.edu>
2017-12-01 17:10:12 -0800

    removed prog2-2 and converted to prog2

--- prog2.cpp
+++ prog2.cpp
@@ -9,38 +9,47 @@ using namespace std;
 
 class Graph {
 private:
-    vector<pair<int, edge> > G; // The graph
-    vector<pair<int, edge> > T; // The MST
-    int *parent;
-    int V; // The number of nodes in the graph
-    vector<int> nodes;
-    vector<int> weights;
+  vector<pair<int, edge> > G; //graph x,y
+  vector<pair<int, edge> > MST; //minimum tree x,y
+  int *parent;
+  int *rank;
+  int nodenum;
+  vector<int> nodes;
+  vector<int> weights;
+  vector<int> m_weights;
+  size_t current;
+  int minCost;
 public:
-    Graph(int V);
-    void AddWeightedEdge(int u, int v, int w);
-    int find_edge(int i);
-    void union_edge(int u, int v);
-    void kruskalMST(bool print_cost);
-    void print_MST();
-    void NumberOfNodes();
-    void CheckForCycle();
-    void ShortestPath(int u, int v, bool first);
+  Graph(int nodenum);
+  void AddWeightedEdge(int u, int v, int w, bool m);
+  int find_edge(int i);
+  void union_edge(int u, int v);
+  void kruskalMST(bool print_cost);
+  void print_MST();
+  void NumberOfNodes();
+  void CheckForCycle();
+  void ShortestPath(int u, int v, bool first);
+  void link(int x, int y);
 };
 
-Graph::Graph(int V) {
-    parent = new int[V];
+Graph::Graph(int nodenum) {
+    parent = new int[nodenum];
+    rank = new int [nodenum];
 
-    for (int i = 0; i < V; i++) {
+    for (int i = 0; i < nodenum; i++) {
 	parent[i] = i;
+    	rank[i] = 0;
     }
 
     G.clear();
-    T.clear();
+    MST.clear();
+    current = 0;
+    minCost = 0;
 }
 
-void Graph::AddWeightedEdge(int u, int v, int w) {
+void Graph::AddWeightedEdge(int u, int v, int w, bool m) {
     if (u < v) {
-	G.push_back(make_pair(w, edge(u, v)));
+      G.push_back(make_pair(w, edge(u, v)));
     } else {
  	G.push_back(make_pair(w, edge(v, u)));
     }
@@ -49,18 +58,33 @@ void Graph::AddWeightedEdge(int u, int v, int w) {
 	nodes.push_back(v);
     }
     weights.push_back(w);
+    if (m) {
+	m_weights.push_back(w);
+    }
 }
 
 int Graph::find_edge(int i) {
     if (i == parent[i]) {
 	return i;
     } else {
-      return find_edge(parent[i+1]);
+	return find_edge(parent[i]);
+    }
+}
+
+void Graph::link(int x, int y) {
+    if (x != y) {
+    	if (rank[x] < rank[y]) {
+	    rank[y] += rank[x];
+	    parent[x] = y;
+	} else {
+	    rank[x] += rank[y];
+	    parent[y] = x;
+	}
     }
 }
 
 void Graph::union_edge(int u, int v) {
-    parent[u] = parent[v];
+    link(find_edge(u), find_edge(v));
 }
 
 void Graph::NumberOfNodes() {
@@ -82,39 +106,44 @@ void Graph::CheckForCycle() {
 void Graph::kruskalMST(bool print_cost) {
     int uRep, vRep;
     size_t i;
-    int cost, minCost = 0;
-    sort(G.begin(), G.end()); // Sort by weight;
-    if (T.size() != 0) {
-    	for (int j = 0; j < MAX_NODE; j++) {
-	    parent[j] = j;
-	}
-	T.clear();
-    }
-    for (i = 0; i < G.size(); i++) {
+    int cost;
+    
+    sort(G.begin()+current, G.end()); //Take weight, push, pop
+    
+    for (i = current; i < G.size(); i++) {
 	uRep = find_edge(G[i].second.first);
     	vRep = find_edge(G[i].second.second);
 	cost = G[i].first;
-	if (uRep != vRep) {
+	if (std::find(m_weights.begin(), m_weights.end(), cost) != m_weights.end()) {
 	    minCost += cost;
-	    T.push_back(G[i]); 
+	    MST.push_back(G[i]);
 	    union_edge(uRep, vRep);
-	}
+        } else {
+	    if (uRep != vRep) {
+	        minCost += cost;
+	        MST.push_back(G[i]); 
+	        union_edge(uRep, vRep);
+	    }
+ 	}
     }
     if (print_cost) {
 	cout << minCost << endl;
     }
+    current = weights.size();
 }
 
 void Graph::ShortestPath(int u, int v, bool first) {
     if (first) {
-	kruskalMST(false);
+	if (current != weights.size()) {
+	    kruskalMST(false);
+	}
  	cout << u << " ";
     }
     int temp;
     temp = u;
-    for (size_t i = 0; i < T.size(); i++) { //ASDF
-    	if (T[i].second.first == temp) {
-	    temp = T[i].second.second;
+    for (size_t i = 0; i < MST.size(); i++) {
+    	if (MST[i].second.first == temp) {
+	    temp = MST[i].second.second;
 	    cout << temp << " ";
     	}
 	if (temp == v) {
@@ -128,9 +157,11 @@ void Graph::ShortestPath(int u, int v, bool first) {
 }
 
 void Graph::print_MST() {
-    kruskalMST(false);
-    for (size_t i = 0; i < T.size(); i++) {
-	cout << "(" << T[i].second.first << "," << T[i].second.second << ")" << " ";
+    if (current != weights.size()) {
+	kruskalMST(false);
+    }
+    for (size_t i = 0; i < MST.size(); i++) {
+	cout << "(" << MST[i].second.first << "," << MST[i].second.second << ")" << " ";
     }
     cout << endl;
 }
@@ -154,19 +185,6 @@ vector<string> parse_line(const string& target, const string& delimiter)
     return result;
 }
 
-void print_menu() {
-    cout << endl;
-    cout << "The following commands are available: " << endl;
-    cout << "  BID node_number node_number cost" << endl;
-    cout << "  MANDATORY node_number node_number cost" << endl;
-    cout << "  COST?" << endl;
-    cout << "  COMPONENTS?" << endl;
-    cout << "  CYCLE?" << endl;
-    cout << "  LIST?" << endl;
-    cout << "  SHORTEST_PATH? node_number node_number" << endl;
-    cout << "  END" << endl;
-}
-
 int main() {
     Graph g(MAX_NODE);
     string line;
@@ -174,15 +192,19 @@ int main() {
     int node_1, node_2, cost;
     size_t i;
 
-    print_menu();
     do {
  	getline(std::cin, line);
      	cmd_line_split_str = parse_line(line, " ");
-	if ((cmd_line_split_str[0] == "BID") || (cmd_line_split_str[0] == "MANDATORY")) {
+	if (cmd_line_split_str[0] == "BID") { //difference between bid and mandatory is one has weight, other does not
 	    node_1 = atoi(cmd_line_split_str[1].c_str());
 	    node_2 = atoi(cmd_line_split_str[2].c_str());
 	    cost = atoi(cmd_line_split_str[3].c_str());
-	    g.AddWeightedEdge(node_1, node_2, cost);
+	    g.AddWeightedEdge(node_1, node_2, cost, false);
+        } else if (cmd_line_split_str[0] == "MANDATORY") { //adds weight and stores it
+    	    node_1 = atoi(cmd_line_split_str[1].c_str());
+	    node_2 = atoi(cmd_line_split_str[2].c_str());
+	    cost = atoi(cmd_line_split_str[3].c_str());
+	    g.AddWeightedEdge(node_1, node_2, cost, true);
 	} else if (cmd_line_split_str[0] == "COST?") {
 	    g.kruskalMST(true);
         } else if (cmd_line_split_str[0] == "COMPONENTS?") {
@@ -190,17 +212,16 @@ int main() {
 	} else if (cmd_line_split_str[0] == "CYCLE?") {
 	    g.CheckForCycle();
 	} else if (cmd_line_split_str[0] == "LIST?") {
-	    g.print_MST();
+	    g.print_MST(); // pull up the search tree
 	} else if (cmd_line_split_str[0] == "SHORTEST_PATH?") {
 	    node_1 = atoi(cmd_line_split_str[1].c_str());
 	    node_2 = atoi(cmd_line_split_str[2].c_str());
 	    cost = atoi(cmd_line_split_str[3].c_str());
 	    g.ShortestPath(node_1, node_2, true);
 	} else if (cmd_line_split_str[0] == "END") {
-	    // Do nothing here.
+	  //call just to end, no need to do anything
 	} else {
 	    cout << line << " is invalid." << endl;
-	    print_menu();
 	}
     } while (cmd_line_split_str[0] != "END");
     return 0;
